797f8acbe4bd3847b2480f612cff8dc049d854d3
Kim Berninger <kimberninger@me.com>
2020-01-20 11:25:09 +0100

    Added pretrained model shortcut

--- squeezedet/models/pretrained/pretrained.py
+++ squeezedet/models/pretrained/pretrained.py
@@ -3,11 +3,19 @@ from pkg_resources import resource_filename
 from squeezedet.layers import Fire
 
 
-def squeezedet_pretrained():
-    filename = resource_filename(
+def load_model(resource_name):
+    file_name = resource_filename(
         'squeezedet.models.pretrained',
-        'squeezedet_pretrained.h5')
+        resource_name)
     return tfk.models.load_model(
-        filename,
+        file_name,
         custom_objects={'Fire': Fire},
         compile=False)
+
+
+def squeezedet_pretrained():
+    return load_model('squeezedet_pretrained.h5')
+
+
+def squeezedet_trained():
+    return load_model('squeezedet_trained.h5')
